3f68fdb165fa9f755242389bc958b1b2935994ed
Alexander <shantyrqq@gmail.com>
2021-07-11 18:40:50 +0300

    Initial Commit

---
+++ apple-healthkit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  apple-healthkit
+//
+//  Created by Alexander Shantyr on 7/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apple-healthkit/apple_healthkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_healthkitApp.swift
+//  apple-healthkit
+//
+//  Created by Alexander Shantyr on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct apple_healthkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
